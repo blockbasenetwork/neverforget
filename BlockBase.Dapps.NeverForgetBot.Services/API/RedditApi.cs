01d980c4a1bfd301f7983790b1f9608b2dfe564c
Jacinto <jjacintow@gmail.com>
2021-03-02 16:08:31 +0000

    reddit api test

--- BlockBase.Dapps.NeverForgetBot.Services/API/RedditApi.cs
+++ BlockBase.Dapps.NeverForgetBot.Services/API/RedditApi.cs
@@ -1,21 +1,22 @@
-﻿namespace BlockBase.Dapps.NeverForgetBot.Services.API
+﻿using Reddit;
+using Reddit.Controllers;
+using Reddit.Inputs.Search;
+using System.Collections.Generic;
+
+namespace BlockBase.Dapps.NeverForgetBot.Services.API
 {
     public class RedditApi
     {
+        public static void AuthenticateClient()
+        {
+            var reddit = new RedditClient("z_FR2DzDnW3EEw", "YourBotUserRefreshToken");
+
+            List<Post> posts = reddit.Subreddit("MySub").Search(new SearchGetSearchInput("Bernie Sanders"));  // Search r/MySub
+            if (posts.Count == 0)
+            {
+                posts = reddit.Subreddit("all").Search(new SearchGetSearchInput("Bernie Sanders"));  // Search r/all
+            }
+        }
 
     }
 }
-
-
-//public static TwitterClient Client { get; set; }
-
-//private static readonly string _CONSUMER_KEY = "9qQx8bvfeJcPqdJl2DRgcsmcU";
-//private static readonly string _CONSUMER_SECRET = "QD9iYlaRvHyz0795Ba2R8Mv02PV62F7zqzPubhn48VclIt78zv";
-//private static readonly string _ACCESS_TOKEN = "1351921561155072002-aVv7lXhYNn8DKWMuTISpQzG1It1slU";
-//private static readonly string _ACCESS_TOKEN_SECRET = "M6DPxMJyzgzT2oci5Z5c1T5Y70ZwtInYtwvTdp1GU9fiq";
-
-//public static void AuthenticateClient()
-//{
-//    //Client = new TwitterClient("CONSUMER_KEY", "CONSUMER_SECRET", "ACCESS_TOKEN", "ACCESS_TOKEN_SECRET");
-//    Client = new TwitterClient(_CONSUMER_KEY, _CONSUMER_SECRET, _ACCESS_TOKEN, _ACCESS_TOKEN_SECRET);
-//}
