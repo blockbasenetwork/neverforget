8b7161ba020a54b3881778050346b344e830d22a
Carlos Garcia <carlosrologarcia@gmail.com>
2021-04-22 15:07:19 +0100

    Added Reddit Pocos in Data layer

---
+++ Data/Pocos/RedditContextPoco.cs
@@ -0,0 +1,17 @@
+﻿using BlockBase.Dapps.NeverForget.Data.Entities;
+using System.Collections.Generic;
+
+namespace BlockBase.Dapps.NeverForget.Data.Pocos
+{
+    public class RedditContextPoco
+    {
+        public virtual RedditContextPoco Context { get; set; }
+        public virtual RedditSubmission Submission { get; set; }
+        public virtual List<RedditComment> Comments { get; set; }
+
+        public RedditContextPoco()
+        {
+            Comments = new List<RedditComment>();
+        }
+    }
+}
