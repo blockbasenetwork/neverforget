11141d18f6f1f414b4e9e98cbb8813eca9517796
Diogo Reis <diogo.reis@adentis.pt>
2021-05-06 15:34:59 +0100

    Twitter/Home controller

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -1,33 +1,51 @@
-﻿using BlockBase.Dapps.NeverForget.WebApp.Models;
+﻿using BlockBase.Dapps.NeverForget.Business.Interfaces;
+using BlockBase.Dapps.NeverForget.WebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace BlockBase.Dapps.NeverForget.WebApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IGeneralContextBusinessObject _generalContextBusinessObject;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IGeneralContextBusinessObject generalContextBusinessObject)
         {
             _logger = logger;
+            _generalContextBusinessObject = generalContextBusinessObject;
         }
 
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var recentCallsList = await _generalContextBusinessObject.GetRecentCalls();
+            if (!recentCallsList.Success) return Error(recentCallsList.Exception.Message);
+
+            var recentList = new List<GeneralContextViewModel>();
+            foreach (var call in recentCallsList.Result)
+            {
+                recentList.Add(GeneralContextViewModel.FromData(call));
+            }
+
+            string logoUrl = Url.Content("~/img/Logo.png");
+            ViewData["Logo"] = logoUrl;
+
+            return View(recentList);
         }
 
-        public IActionResult Privacy()
+        public IActionResult Error(string message)
         {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            _logger.LogError(message);
+            var viewModel = new ErrorViewModel
+            {
+                Message = message,
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            };
+            return Error(viewModel.Message);
         }
     }
 }
---
+++ WebApp/Controllers/TwitterContextsController.cs
@@ -0,0 +1,59 @@
+﻿using BlockBase.Dapps.NeverForget.Business.Interfaces;
+using BlockBase.Dapps.NeverForget.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlockBase.Dapps.NeverForget.WebApp.Controllers
+{
+    public class TwitterContextsController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ITwitterContextBusinessObject _twitterContextBusinessObject;
+
+        public TwitterContextsController(ILogger<HomeController> logger, ITwitterContextBusinessObject twitterContextBusinessObject)
+        {
+            _logger = logger;
+            _twitterContextBusinessObject = twitterContextBusinessObject;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var twitterContextsList = await _twitterContextBusinessObject.GetAllPocoAsync();
+            if (!twitterContextsList.Success) return View("Error", new ErrorViewModel() { RequestId = twitterContextsList.Exception.Message });
+
+            var contextsList = new List<TwitterContextViewModel>();
+            foreach (var context in twitterContextsList.Result)
+            {
+                contextsList.Add(TwitterContextViewModel.FromData(context));
+            }
+
+            var orderedList = contextsList.OrderByDescending(i => i.Date);
+
+            string logoUrl = Url.Content("~/img/twitterRobot.png");
+            ViewData["Logo"] = logoUrl;
+
+            return View(orderedList);
+        }
+
+        [HttpGet("TwitterContexts/Details/{id}")]
+        public async Task<IActionResult> Details(Guid? contextId)
+        {
+            if (contextId == null) return NotFound();
+
+            var detailedContext = await _twitterContextBusinessObject.GetPocoAsync((Guid)contextId);
+            if (!detailedContext.Success) return View("Error", new ErrorViewModel() { RequestId = detailedContext.Exception.Message });
+
+            if (detailedContext.Result == null) return NotFound();
+
+            string logoUrl = Url.Content("~/img/twitterRobot.png");
+            ViewData["Logo"] = logoUrl;
+
+            return View(TwitterDetailsViewModel.FromData(detailedContext.Result));
+        }
+    }
+}
